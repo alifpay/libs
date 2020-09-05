2e2933cf7308ad15f2d1984733f8d2633c9a8ca9
Alisher <sambusa001@mail.ru>
2020-09-05 15:03:48 +0500

    initial commit

---
+++ errcodes/errcodes.go
@@ -0,0 +1,25 @@
+package errcodes
+
+const (
+	TransactionNotExists = 104
+	TransactionAlreadyExists = 107
+	SuccessDuplicate = 108
+	Success = 200
+	InProcessing = 201
+	AcceptedToProcessing = 202
+	Rejected = 203
+	CheckRequestSuccessful = 301
+	AccountFound = 302
+	ServiceTemporarilyUnavailable = 303
+	DoNotTryAgain = 304
+	InsufficientFundsOnTheAccount = 305
+	BadRequest = 400
+	NotFound = 404
+	AmountOutOfRange = 405
+	CheckRequestFailed = 406
+	InternalServerError = 500
+	ServiceUnavailable = 503
+	UnknownError = 520
+	ProviderConnectionFailed = 521
+	BadProviderResponse = 523
+)
